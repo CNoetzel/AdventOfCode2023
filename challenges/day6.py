afd44f597e67bb2bdf88022f86ee819ace98e07d
Carsten Noetzel <carsten.noetzel@tchibo.de>
2023-12-06 08:32:53 +0100

    refactor function names

--- challenges/day6.py
+++ challenges/day6.py
@@ -6,7 +6,7 @@ def win_possibilities(time: int, distance_to_beat: int) -> List[int]:
     return [h for h in range(time) if h * (time - h) > distance_to_beat]
 
 
-def product_of_ways_to_win(data: List[str]) -> int:
+def ways_to_win(data: List[str]) -> int:
     time_data = list(map(int, data[0].replace("Time:", "").strip().split()))
     distance_data = list(map(int, data[1].replace("Distance:", "").strip().split()))
 
@@ -16,7 +16,7 @@ def product_of_ways_to_win(data: List[str]) -> int:
     return result
 
 
-def product_of_ways_to_win_single_race(data: List[str]) -> int:
+def ways_to_win_single_race(data: List[str]) -> int:
     time_data = int(re.sub(r"(Time:|\s)", "", data[0]))
     distance_data = int(re.sub(r"(Distance:|\s)", "", data[1]))
     return len(win_possibilities(time_data, distance_data))
@@ -26,10 +26,8 @@ def main():
     f = open("challenges/day6_input.txt", "r")
     lines = f.readlines()
 
-    print(f"Task 1: The product of ways to win is {product_of_ways_to_win(lines)}")
-    print(
-        f"Task 2: The product of ways to win is {product_of_ways_to_win_single_race(lines)}"
-    )
+    print(f"Task 1: The product of ways to win is {ways_to_win(lines)}")
+    print(f"Task 2: The product of ways to win is {ways_to_win_single_race(lines)}")
 
 
 if __name__ == "__main__":
--- tests/test_day6.py
+++ tests/test_day6.py
@@ -4,10 +4,10 @@ test_input = ["Time:      7  15   30", "Distance:  9  40  200"]
 
 
 def test_task1():
-    result = day6.product_of_ways_to_win(test_input)
+    result = day6.ways_to_win(test_input)
     assert result == 288
 
 
 def test_task2():
-    result = day6.product_of_ways_to_win_single_race(test_input)
+    result = day6.ways_to_win_single_race(test_input)
     assert result == 71503
