4709e4d1691f94fcc66ae722798a0aa0ba1961fa
Carsten Noetzel <carsten.noetzel@tchibo.de>
2023-12-07 19:32:05 +0100

    solution day 7 part 2

--- challenges/day7.py
+++ challenges/day7.py
@@ -1,7 +1,11 @@
 from typing import List
 from enum import Enum
 
+# global flag for controlling if Js should be considered as jokers and card values
+# for the two different cases
+js_are_jokers = False
 cards_value = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+cards_value_jokers = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
 
 
 class Type(Enum):
@@ -14,27 +18,69 @@ class Type(Enum):
     FIVE_OF_A_KIND = 7
 
 
+# Mapping of types if the hand contains jokers
+# HIGH_CARD with one joker will become ONE_PAIR, ...
+joker_mapping = {
+    "HIGH_CARD": [
+        Type.ONE_PAIR,
+        Type.THREE_OF_A_KIND,
+        Type.FOUR_OF_A_KIND,
+        Type.FIVE_OF_A_KIND,
+    ],
+    "ONE_PAIR": [
+        Type.THREE_OF_A_KIND,
+        Type.FOUR_OF_A_KIND,
+        Type.FIVE_OF_A_KIND,
+    ],
+    "TWO_PAIR": [
+        Type.FULL_HOUSE,
+    ],
+    "THREE_OF_A_KIND": [
+        Type.FOUR_OF_A_KIND,
+        Type.FIVE_OF_A_KIND,
+    ],
+    "FULL_HOUSE": [Type.FULL_HOUSE],
+    "FOUR_OF_A_KIND": [Type.FIVE_OF_A_KIND],
+}
+
+
+# Class containing the logic for determining type of hand
+# and algorithm to sort hands
 class Hand:
     def __init__(self, cards: str, bid: int):
         self.cards = cards
         self.bid = bid
-        self.type = self.determine_type()
+        self.type = (
+            self.determine_type_with_jokers()
+            if js_are_jokers
+            else self.determine_type(self.cards, 0)
+        )
 
     def has_same_card_times(self, times: int) -> bool:
         return any(self.cards.count(card) == times for card in self.cards)
 
-    def determine_type(self):
-        individual_cards = len(set(self.cards))
-        if individual_cards == 5:
+    def determine_type_with_jokers(self) -> Type:
+        number_of_jokers = self.cards.count("J")
+        if number_of_jokers == 0:
+            return self.determine_type(self.cards, 0)
+        elif number_of_jokers == 5:
+            return Type.FIVE_OF_A_KIND
+        sanitized_cards = self.cards.replace("J", "")
+        base_type = self.determine_type(sanitized_cards, -number_of_jokers)
+        return joker_mapping[base_type.name][number_of_jokers - 1]
+
+    def determine_type(self, cards: str, offset: int) -> Type:
+        individual_cards = len(set(cards))
+        if individual_cards == 5 + offset:
             return Type.HIGH_CARD
-        elif individual_cards == 4:
+        elif individual_cards == 4 + offset:
             return Type.ONE_PAIR
-        elif individual_cards == 3:
+        elif individual_cards == 3 + offset:
             # THREE_OF_A_KIND or TWO_PAIR
             if self.has_same_card_times(3):
                 return Type.THREE_OF_A_KIND
             return Type.TWO_PAIR
-        elif individual_cards == 2:
+        elif individual_cards == 2 + offset:
             # FOUR_OF_A_KIND or FULL_HOUSE
             if self.has_same_card_times(4):
                 return Type.FOUR_OF_A_KIND
@@ -46,18 +92,21 @@ class Hand:
         return f"Hand({self.cards}, {self.bid}, {self.type})"
 
     def __lt__(self, other):
+        value_list = cards_value_jokers if js_are_jokers else cards_value
         if self.type == other.type:
             for idx in range(5):
                 if self.cards[idx] == other.cards[idx]:
                     continue
-                return cards_value.index(self.cards[idx]) < cards_value.index(
+                return value_list.index(self.cards[idx]) < value_list.index(
                     other.cards[idx]
                 )
             return False
         return self.type.value < other.type.value
 
 
-def total_winnings(data: List[str]) -> int:
+def total_winnings(data: List[str], jokers: bool) -> int:
+    global js_are_jokers
+    js_are_jokers = jokers
     hands = [Hand(line.split(" ")[0], int(line.split(" ")[1])) for line in data]
     hands.sort()
     return sum((idx + 1) * hand.bid for idx, hand in enumerate(hands))
@@ -67,11 +116,8 @@ def main():
     f = open("challenges/day7_input.txt", "r")
     lines = f.readlines()
 
-    print(f"Task 1: The total winnings are {total_winnings(lines)}")
-    # too high 251032781
-    # too high 250691712
-    # too high 250518356
-    # print(f"Task 2: The product of ways to win is {ways_to_win_single_race(lines)}")
+    print(f"Task 1: The total winnings are {total_winnings(lines, False)}")
+    print(f"Task 2: The total winnings are {total_winnings(lines, True)}")
 
 
 if __name__ == "__main__":
--- tests/test_day7.py
+++ tests/test_day7.py
@@ -4,10 +4,10 @@ test_input = ["32T3K 765", "T55J5 684", "KK677 28", "KTJJT 220", "QQQJA 483"]
 
 
 def test_task1():
-    result = day7.total_winnings(test_input)
+    result = day7.total_winnings(test_input, False)
     assert result == 6440
 
 
-# def test_task2():
-#    result = day6.ways_to_win_single_race(test_input)
-#    assert result == 71503
+def test_task2():
+    result = day7.total_winnings(test_input, True)
+    assert result == 5905
