1c25654688afd3843c9ca37f69246d772b8181b9
Carsten Noetzel <carsten.noetzel@tchibo.de>
2023-12-10 12:41:41 +0100

    remove uncommented lines

--- tests/test_day10.py
+++ tests/test_day10.py
@@ -12,8 +12,3 @@ test_input = [
 def test_task1():
     result = day10.steps_to_farthest_position(test_input)
     assert result == 8
-
-
-# def test_task2():
-#    result = day9.sum_sequence_values(test_input)
-#    assert result == 2
